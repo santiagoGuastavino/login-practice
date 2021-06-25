923aabfcc7b67474526384401c471f0f36d86435
santiagoGuastavino <san.guasta@gmail.com>
2021-06-25 01:29:18 -0300

    express validator 100%

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,14 +1,22 @@
+let { validationResult } = require('express-validator');
+
 let mainController = {
     home: (req,res) => {
         res.render('index')
     },
 
-    user: (req,res) => {
-        let user = {
-            ...req.body
+    user: (req,res) => {        
+        let validations = validationResult(req);
+        if (validations.errors.length > 0) {
+            let oldData = req.body;
+            res.render('index', { oldData, 'errors':validations.mapped() } );
+        }else{
+            let user = {
+                ...req.body
+            };
+            req.session.user = user;
+            res.redirect('/');
         };
-        req.session.user = user;
-        res.redirect('/');
     },
 };
 
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,8 +1,22 @@
 let express = require('express');
 let router = express.Router();
+let { check } = require('express-validator')
 let mainController = require('../controllers/mainController');
 
+let validations = [
+    check('name')
+        .notEmpty().withMessage('Debes completar éste campo').bail(),
+    check('color')
+        .not().contains('#ffffff').withMessage('Debes elegir un color').bail(),
+    check('email')
+        .notEmpty().withMessage('Debes completar éste campo').bail()
+        .normalizeEmail().isEmail().withMessage('Debes ingresar una dirección de e-mail válida').bail(),
+    check('age')
+        .notEmpty().withMessage('Debes completar éste campo').bail()
+        .isInt().withMessage('Debes completar con un número').bail(),
+];
+
 router.get('/', mainController.home);
-router.post('/', mainController.user);  
+router.post('/', validations, mainController.user);  
 
 module.exports = router;
